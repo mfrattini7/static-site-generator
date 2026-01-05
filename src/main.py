1a087ef5f2ce79e8159cdc7481beb53b03ce8159
mfrattini7 <marcello.frattini7@gmail.com>
2026-01-05 15:00:12 +0100

    rewrite

--- src/main.py
+++ src/main.py
@@ -1,7 +1,9 @@
-from textnode import TextNode
+from textnode import TextNode, TextType
+
 
 def main():
-    tn = TextNode("This is some anchor text", "link", "https://www.boot.dev")
-    print(tn)
+    node = TextNode("This is a text node", TextType.BOLD, "https://www.boot.dev")
+    print(node)
+
 
 main()
--- src/test_text_node_to_html_node.py
+++
@@ -1,8 +0,0 @@
-from textnode import TextNode, TextType
-from text_node_to_html_node import text_node_to_html_node
-
-def test_text(self):
-    node = TextNode("This is a text node", TextType.TEXT)
-    html_node = text_node_to_html_node(node)
-    self.assertEqual(html_node.tag, None)
-    self.assertEqual(html_node.value, "This is a text node")
